630ed4ac1fe0b51c08f70842d7386c7fd07a897c
Lino <masterlino@gmail.com>
2020-07-14 11:08:11 +0200

    Project Creation
    Adding a splash screen, marvel font.

---
+++ Marvelios/Marvelios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Marvelios
+//
+//  Created by Lino Bustamante on 14/07/2020.
+//  Copyright © 2020 Bustamante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
